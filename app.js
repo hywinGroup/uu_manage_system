06967ddff7baa2793eb58ff0bf1016cccf6af8a8
susie <susiewang163@163.com>
2017-08-15 09:38:45 +0800

    添加新页面

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict'
+App({
+  onLaunch: function(options) { 
+
+  },
+  onShow: function(options) {
+      // Do something when show.
+  },
+  onHide: function() {
+      // Do something when hide.
+  },
+  onError: function(msg) {
+    console.log(msg)
+  },
+  globalData: 'I am global data'
+})
